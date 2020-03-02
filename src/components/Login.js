7912d8d33be30aabb887882458b5b40caab8a6eb
Owner <owner@Users-MacBook-Pro.local>
2020-03-01 23:15:02 -0500

    login component created. Will be completed with redux.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleChange = event => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  const handleLoginSubmit = event => {
+    event.preventDefault();
+    console.log("Some Redux Action is About to Go On Here");
+  };
+  // console.log(user);
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className="login-page-container">
+      <Header />
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={handleLoginSubmit}>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            id="name"
+            onChange={event => handleChange(event)}
+            value={user.userName}
+          />
+          <label>Password</label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            onChange={event => handleChange(event)}
+            value={user.password}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
     </div>
   );
 };

835be97dab4205c3e2eef3335b2421dd278b1d4f
sametweb <sametmutevellioglu@gmail.com>
2020-03-02 20:34:22 -0800

    Overall styling, Login/Signup functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,33 +4,35 @@ import Header from "./Header";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ height: "100%" }}>
       <Header />
-      <div className="home-container">
-        <div className="search-template">
-          <i class="fa fa-search"></i> <span> Find Music</span>
-          <p>
-            {" "}
-            Just tell us how you are feeling and we got you! Finding music by
-            mood has never beene asier. Either in a dance mood or getting over
-            breakup, we have something to play for you.
-          </p>
+      <div className="home-full-bg">
+        <div className="home-container">
+          <div className="search-template">
+            <i className="fa fa-search"></i> <span> Find Music</span>
+            <p>
+              {" "}
+              Just tell us how you are feeling and we got you! Finding music by
+              mood has never beene asier. Either in a dance mood or getting over
+              breakup, we have something to play for you.
+            </p>
+          </div>
+          <div className="favorite-template">
+            <i className="fas fa-heart"></i> <span> Save Your Favorites</span>
+            <p>
+              {" "}
+              When you like a song, simply click the like button and we will
+              save it to your favorite songs list You can come back and listen
+              to your favorite songs whenenever you want!
+            </p>
+          </div>
         </div>
-        <div className="favorite-template">
-          <i class="fas fa-heart"></i> <span> Save Your Favorites</span>
-          <p>
-            {" "}
-            When you like a song, simply click the like button and we will save
-            it to your favorite songs list You can come back and listen to your
-            favorite songs whenenever you want!
-          </p>
-        </div>
+        <footer>
+          <div className="footer-content">
+            <p>Song Surfer Est 2020</p>
+          </div>
+        </footer>
       </div>
-      <footer>
-        <div className="footer-content">
-          <p>Song Surfer Est 2020</p>
-        </div>
-      </footer>
     </div>
   );
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,50 +3,58 @@ import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
 
 import Header from "./Header";
-const SignUp = ({ touched, errors, status }) => {
-  const [user, setUser] = useState("");
+import { axiosWithAuth } from "../axiosWithAuth";
+
+const SignUp = ({ touched, errors, status, ...props }) => {
+  // const [user, setUser] = useState("");
+
+  // useEffect(() => {
+  //   status && setUser(status);
+  // }, [user, status]);
+
   useEffect(() => {
-    status && setUser(status);
-  }, [user, status]);
-  console.log(user);
+    status?.token && props.history.push("/login");
+  }, [status]);
+
   return (
     <div className="sign-up-page">
       <Header />
       <div className="sign-up-form-container">
         <Form className="sign-up-form">
-          <label>
+          <h1>Sign Up</h1>
+          {/* <label>
             <span>First Name:</span>
             <Field type="text" name="firstName" />
             {touched.firstName && errors.firstName && (
               <p> {errors.firstName}</p>
             )}
-          </label>
+          </label> */}
 
-          <label>
+          {/* <label>
             <span> Last Name:</span>
             <Field type="text" name="lastName" />
             {touched.lastName && errors.lastName && <p> {errors.lastName}</p>}
-          </label>
-
+          </label> */}
+          {status?.error && <div className="error">{status.error}</div>}
           <label>
-            <span>Email:</span>
-            <Field type="text" name="email" />
-            {touched.email && errors.email && <p> {errors.email}</p>}
+            <span>Username:</span>
+            <Field type="text" name="username" />
+            {touched.username && errors.username && <p>{errors.username}</p>}
           </label>
 
           <label>
             <span>Password:</span>
-            <Field type="text" name="password" />
-            {touched.password && errors.password && <p> {errors.password}</p>}
+            <Field type="password" name="password" />
+            {touched.password && errors.password && <p>{errors.password}</p>}
           </label>
           <button type="submit"> Sign Up</button>
         </Form>
-        {user && (
+        {/* {user && (
           <div className="dynamic-welcome">
             {" "}
             Welcome to Song Surfer {user.firstName}!{" "}
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
@@ -54,25 +62,27 @@ const SignUp = ({ touched, errors, status }) => {
 
 export default withFormik({
   mapPropsToValues: props => ({
-    firstName: "",
-    lastName: "",
-    email: "",
+    // firstName: "",
+    // lastName: "",
+    username: "",
     password: ""
   }),
   validationSchema: yup.object().shape({
-    firstName: yup.string().required("Please Supply Your First Name"),
-    lastName: yup.string().required("Please Supply Your First Name"),
-    email: yup
-      .string()
-      .required("Please Supply A Valid Email Address")
-      .email(),
+    // firstName: yup.string().required("Please Supply Your First Name"),
+    // lastName: yup.string().required("Please Supply Your First Name"),
+    username: yup.string().required("* Please Supply A Valid Username"),
     password: yup
       .string()
-      .required("Please Supply A Password With A Minumum Of 8 Characters")
+      .required("* Please Supply A Password With A Minumum Of 8 Characters")
       .min(8)
   }),
   handleSubmit: (values, { resetForm, setStatus }) => {
-    setStatus(values);
+    axiosWithAuth()
+      .post("/register", values)
+      .then(res => setStatus({ token: res.data.token }))
+      .catch(err =>
+        setStatus({ error: "Error signing up with the credentials provided." })
+      );
     resetForm();
   }
 })(SignUp);
